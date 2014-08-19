f72bd7406e726510e44db9e0eb43ebb2b6737aa7
Jon Abrams <jon@jonabrams.com>
2014-08-19 14:27:33 -0700

    Force latest version in case of bower conflict.
    Fixes #71

--- templates/back/mongojs/resources/tweets/getTweets.js
+++ templates/back/mongojs/resources/tweets/getTweets.js
@@ -1,10 +1,10 @@
 exports.getIndex = function (db) {
   return db.collection('tweets').find()
-    .sort({ created_at: -1 })
-    .limit(30)
-    .toArray().then(function (tweets) {
-      return {
-        tweets: tweets
-      };
-    });
+  .sort({ created_at: -1 })
+  .limit(30)
+  .toArray().then(function (tweets) {
+    return {
+      tweets: tweets
+    };
+  });
 };
