e024af8a52b9c814ae2e8a34a4f76e806d48cf70
Jonathan Abrams <jon@jonabrams.com>
2015-04-10 10:17:48 -0400

    Compile in PR#84

--- js/main.js
+++ js/main.js
@@ -1,16 +1,1 @@
-$(function() {
-  var $window, page;
-  page = location.pathname.replace(/\//g, "");
-  $("." + page).addClass("active");
-  $window = $(window);
-  return $window.on('scroll', function() {
-    if (!($window.width() > 992)) {
-      return;
-    }
-    if ($window.scrollTop() > 80) {
-      return $('.toc').addClass('fixed');
-    } else {
-      return $('.toc').removeClass('fixed');
-    }
-  });
-});
+$(function(){var e,r;r=location.pathname.replace(/\//g,"");$("."+r).addClass("active");e=$(window);return e.on("scroll",function(){if(!(e.width()>992)){return}if(e.scrollTop()>80){return $(".toc").addClass("fixed")}else{return $(".toc").removeClass("fixed")}})});
